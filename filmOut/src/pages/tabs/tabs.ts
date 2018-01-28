c6d23ef4d4e33d608a2fb5d5ad4fbb3324e21dbe
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-01-28 12:17:49 +0100

    Applied lazy loading, so each page has its own URL (deep-links). The server must be reinitiated for changes to take effect.

--- filmOut/src/pages/tabs/tabs.ts
+++ filmOut/src/pages/tabs/tabs.ts
@@ -3,17 +3,21 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { IonicPage } from 'ionic-angular';
 
 import { PROFILE_PAGE } from "../pages.constants";
 
+@IonicPage({
+  segment: 'tabs-page'  // Importante. Para que recargue la página en la cual estamos
+})
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = 'HomePage';
+  tab2Root = 'AboutPage';
+  tab3Root = 'ContactPage';
   tab4Root = PROFILE_PAGE;
 
   constructor() {
